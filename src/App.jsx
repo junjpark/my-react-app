1d523e6ad8df29145f13340b9219d933d9dc46ae
Justin Park <junjpark02@gmail.com>
2024-11-12 12:11:20 -0600

    -Fixed minor typo that caused a bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,26 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board({ xIsNext, squares, onPlay }) {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    const nextSquares = squares.slice(); //takes whole square array
+    const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
       nextSquares[i] = 'O';
     }
     onPlay(nextSquares);
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
   }
 
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
-  }
-  else {
-    status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
@@ -61,7 +55,7 @@ export default function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSqaures = history[history.length - 1];
+  const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]);
@@ -69,19 +63,18 @@ export default function Game() {
   }
 
   function jumpTo(nextMove) {
-
+    // TODO
   }
 
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
       description = 'Go to move #' + move;
-    }
-    else {
+    } else {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
@@ -108,7 +101,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
@@ -118,3 +111,4 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
